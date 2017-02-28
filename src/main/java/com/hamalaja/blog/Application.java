425df3d2f28ff53eea722cc7a2b425a0dae53ce4
hamalaja <hamalajava@gmail.com>
2017-02-28 23:10:16 +0700

    integrate thymeleaf

--- src/main/java/com/hamalaja/blog/Application.java
+++ src/main/java/com/hamalaja/blog/Application.java
@@ -1,9 +1,15 @@
 package com.hamalaja.blog;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author LamHa
  *
  */
+@SpringBootApplication
 public class Application {
-
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
